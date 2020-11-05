2370e5de38250fcaf83b501ef3ffea20783ebc0f
thientran1199 <thientran1111999@gmail.com>
2020-11-05 08:51:42 +0700

    construct

--- MyClass.php
+++ MyClass.php
@@ -1,7 +1,19 @@
-<?
+<?php
 class MyClass{
-    public function __construct(...$arg){
-        var_dump($arg);
+    public function __construct(...$args){
+        var_dump($args);
     } 
+    public function __destruct()
+    {
+        echo"khi doi tuong bi huy";
+    }
+    public function __set($key, $value)
+    {
+        echo "key: $key  -  value: $value ";
+    }
+    public function __get($key)
+    {
+        echo "thuoc tinh vua goi  $key ";
+    }
+
 }
-?>
--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-    include MyClass.php;
-?>
+include 'MyClass.php';
+$hello = new MyClass(array(
+    'name'=> 'MyName',
+    'pass' => '123',
+));
+
+$hello->name ="MyName";
+$hello->name;
+
